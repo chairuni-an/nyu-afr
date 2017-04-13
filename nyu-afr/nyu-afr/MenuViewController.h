d0c8322db278fbad00506f10eb22a9cdcbf45e35
Alyssa Hsiang <ajh551@nyu.edu>
2017-04-13 12:14:44 -0400

    Menu, Achievements, Profile views added

---
+++ nyu-afr/nyu-afr/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  nyu-afr
+//
+//  Created by Alyssa Hsiang on 4/13/17.
+//  Copyright © 2017 New York University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
