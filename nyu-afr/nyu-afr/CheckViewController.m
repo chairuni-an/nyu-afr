a5c91e5f41debdccc570f9e9e883b30991be35de
Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>
2017-04-26 12:34:00 -0400

    added photo upload

--- nyu-afr/nyu-afr/CheckViewController.m
+++ nyu-afr/nyu-afr/CheckViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "CheckViewController.h"
+#import "AppDelegate.h"
+@import FirebaseStorage;
 
 @interface CheckViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *myImageView;
 @property (weak, nonatomic) IBOutlet UILabel *placenameLabel;
 @property (weak, nonatomic) IBOutlet UIButton *choosePlacenameButton;
 @property (weak, nonatomic) IBOutlet UIButton *submitButton;
+@property (nonatomic) NSURL *pictureDownloadURL;
 @end
 
 @implementation CheckViewController
@@ -29,9 +32,7 @@
 
 - (IBAction)takePhoto:(id)sender
 {
-    NSLog(@"-----Taking Photo!");
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-        NSLog(@"-----Photo Available!");
         UIImagePickerController *picker = [[UIImagePickerController alloc] init];
         picker.delegate = self;
         picker.sourceType = UIImagePickerControllerSourceTypeCamera;
@@ -44,15 +45,87 @@
                          }];
         
     } else if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
-        NSLog(@"-----Library Available!");
         UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
         imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
         imagePickerController.delegate = self;
         imagePickerController.allowsEditing = false;
         [self presentViewController:imagePickerController animated:YES completion:nil];
-    } else {
-        NSLog(@"-----Nothing is Available!");
     }
+
+}
+
+- (IBAction)submitButtonPressed:(id)sender {
+    if (self.myImageView.image != nil) {
+        [self submitAnswer];
+    }
+}
+
+- (void)submitAnswer {
+    NSLog(@"going to submit answer");
+    BOOL isAnswerRight = YES;
+    if (isAnswerRight) {
+        // upload picture
+        AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+        
+        // Data in memory
+        NSLog(@"-----A");
+        NSData *data = UIImageJPEGRepresentation(self.myImageView.image, 0.8);
+        NSLog(@"-----B");
+        
+        // Create file metadata including the content type
+        FIRStorageMetadata *myMetadata = [[FIRStorageMetadata alloc] init];
+        myMetadata.contentType = @"image/jpeg";
+        
+        // Create a root reference
+        FIRStorageReference *storageRef = [delegate.storage reference];
+        
+        // Create a reference to 'user_id/userPhoto/filename.jpg'
+        NSLog(@"-----C");
+
+        NSString *uuid = [[NSUUID UUID] UUIDString];
+        FIRStorageReference *imageRef = [storageRef child:[NSString stringWithFormat:@"%@/userPhoto/%@", [FIRAuth auth].currentUser.uid, uuid]];
+        NSLog(@"-----D");
+
+        // Upload the file to the path "images/rivers.jpg"
+        FIRStorageUploadTask *uploadTask = [imageRef putData:data
+                                                    metadata:myMetadata
+                                                  completion:^(FIRStorageMetadata *metadata,
+                                                               NSError *error) {
+                                                      if (error != nil) {
+                                                          // Uh-oh, an error occurred!
+                                                          NSLog(@"-----aaak");
+
+                                                      } else {
+                                                          // Metadata contains file metadata such as size, content-type, and download URL.
+                                                          self.pictureDownloadURL = metadata.downloadURL;
+                                                          NSLog(@"%@", self.pictureDownloadURL);
+                                                          [self uploadData];
+                                                          
+                                                      }
+                                                  }];
+    }
+}
+
+- (void)uploadData {
+    NSLog(@"-----upload data");
+
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+
+    NSMutableDictionary *temp;
+    [temp setObject:self.pictureDownloadURL forKey:@"selfieURL"];
+    //[temp setObject:key forKey:@"key"]; //later for timestamp
+    
+    NSLog(@"-----bismillah");
+    NSMutableDictionary *currentQuest= [delegate.userModel.userData objectForKey:@"current_quest"];
+    
+    NSString *key = [currentQuest objectForKey:@"key"];
+
+    [[[[[delegate.ref child:@"users"]
+        child:[FIRAuth auth].currentUser.uid]
+        child: @"quests"]
+      child: key]
+        setValue: temp];
+    NSLog(@"-----Binyong");
 
 }
 
@@ -65,9 +138,7 @@
     UIImage * img = [info valueForKey:UIImagePickerControllerOriginalImage]; // you can change it to edited image
     
     self.myImageView.image = img;
-
-     //Or you can get the image url from AssetsLibrary
-     //NSURL *path = [info valueForKey:UIImagePickerControllerReferenceURL];
+    
 }
 
 /*
