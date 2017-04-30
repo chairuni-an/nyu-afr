ed97dd9d9d0f344f0cb369bda5d1a7b3f2889920
Alyssa Hsiang <ajh551@nyu.edu>
2017-04-30 18:43:34 -0400

    updated quest record

--- nyu-afr/nyu-afr/QuestTableViewController.m
+++ nyu-afr/nyu-afr/QuestTableViewController.m
@@ -159,7 +159,19 @@
     cell.questName.text = [_questNameArray objectAtIndex:indexPath.row];
     cell.date.text = [_formattedDateArray objectAtIndex:indexPath.row];
     cell.questNumber.text = [NSString stringWithFormat:@"Quest# %ld -", (long)indexPath.row + 1];
-    cell.selfieImage.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:[_questURLArray objectAtIndex:indexPath.row]]]];
+    
+    dispatch_async(dispatch_get_global_queue(0,0), ^{
+        NSData * data = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: [_questURLArray objectAtIndex:indexPath.row]]];
+        if ( data == nil )
+            return;
+        dispatch_async(dispatch_get_main_queue(), ^{
+        
+            cell.selfieImage.image = [UIImage imageWithData: data];
+        });
+        
+    });
+    
+
     
     return cell;
 }
